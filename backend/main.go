6fcd269704b87968be9434c19bd0191e8c82c4fa
Wooyang Son <sonwy102@gmail.com>
2021-01-11 17:36:02 -0500

    connect server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
 
+// func main() {
+//   // Set the router as the default one shipped with Gin
+//   router := gin.Default()
+
+//   // Serve frontend static files
+//   // router.Use(static.Serve("/", static.LocalFile("./views", true)))
+  
 func main() {
-  fmt.Println("Chat App v0.01")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 }
