7a33a9d860f681d72e36494849a34cbee73f26d1
Wooyang Son <sonwy102@gmail.com>
2021-01-11 18:33:15 -0500

    serve react files

--- backend/go.mod
+++
@@ -1,8 +0,0 @@
-module twitter-spotify-curator
-
-go 1.15
-
-require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/lib/pq v1.9.0 // indirect
-)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+// func main() {
+//   // Set the router as the default one shipped with Gin
+//   router := gin.Default()
+
+//   // Serve frontend static files
+//   // router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+// func main() {
+// 	r := gin.Default()
+// 	r.GET("/", func(c *gin.Context) {
+// 		c.JSON(200, gin.H{
+// 			"message": "pong",
+// 		})
+// 	})
+// 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+// }
+
+func main() {
+  // Set the router as the default one shipped with Gin
+  router := gin.Default()
+  
+  // Serve frontend static files
+  router.Use(static.Serve("/", static.LocalFile("./frontend/build", true)))
+  
+  // Setup route group for the API
+  api := router.Group("/api")
+  {
+    api.GET("/", func(c *gin.Context) {
+      c.JSON(http.StatusOK, gin.H {
+        "message": "pong",
+      })
+    })
+  }
+  
+  // Start and run the server
+  router.Run(":5000")
+}
