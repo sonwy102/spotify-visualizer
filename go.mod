c8b5f6633a6cd2520ea282be6944cb887689392b
Wooyang Son <sonwy102@gmail.com>
2021-01-14 16:05:06 -0500

    test twitter api keys

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module twitter-spotify-curator
 go 1.15
 
 require (
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d // indirect
+	github.com/dghubble/oauth1 v0.7.0 // indirect
 	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/lib/pq v1.9.0
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+
+	"fmt"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 // func main() {
@@ -32,16 +36,28 @@ func main() {
   
   // Serve frontend static files
   router.Use(static.Serve("/", static.LocalFile("./client", true)))
+
+  consumerKey := os.Getenv("TWITTER_API_KEY")
+	consumerSecret := os.Getenv("TWITTER_API_SECRET_KEY")
+	accessToken := os.Getenv("TWITTER_ACCESS_TOKEN")
+	accessSecret := os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
   
-  // Setup route group for the API
-  api := router.Group("/api")
-  {
-    api.GET("/", func(c *gin.Context) {
-      c.JSON(http.StatusOK, gin.H {
-        "message": "pong",
-      })
-    })
-  }
+  config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+
+	// OAuth1 http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	// Verify Credentials
+	verifyParams := &twitter.AccountVerifyParams{
+		IncludeEmail: twitter.Bool(true),
+	}
+	user, _, _ := client.Accounts.VerifyCredentials(verifyParams)
+	fmt.Printf("User's Name:%+v\n", user.ScreenName)
+
   
   // Start and run the server
   router.Run(":5000")
